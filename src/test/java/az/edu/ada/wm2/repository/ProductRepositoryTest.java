2326b87ddb3854e03bfc8447748e4bf5da929f72
sadiq18131 <shasanli18131@ada.edu.az>
2026-04-02 23:58:18 +0400

    Fix Final errors

--- src/test/java/az/edu/ada/wm2/repository/ProductRepositoryTest.java
+++ src/test/java/az/edu/ada/wm2/repository/ProductRepositoryTest.java
@@ -1,6 +1,7 @@
 package az.edu.ada.wm2.repository;
 
 import az.edu.ada.wm2.lab6.model.Product;
+import az.edu.ada.wm2.lab6.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
--- src/test/java/az/edu/ada/wm2/service/CategoryServiceImplTest.java
+++ src/test/java/az/edu/ada/wm2/service/CategoryServiceImplTest.java
@@ -6,8 +6,9 @@ import az.edu.ada.wm2.lab6.model.dto.CategoryRequestDto;
 import az.edu.ada.wm2.lab6.model.dto.CategoryResponseDto;
 import az.edu.ada.wm2.lab6.model.dto.ProductResponseDto;
 import az.edu.ada.wm2.lab6.model.mapper.ProductMapper;
-import az.edu.ada.wm2.lab6.repository.CategoryRepository;
 
+import az.edu.ada.wm2.lab6.repository.CategoryRepository;
+import az.edu.ada.wm2.lab6.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
